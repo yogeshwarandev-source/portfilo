a5021bd3f86f834b37362ab28a5759a2952f575d
yogeshwarandev <yogeshshankar134@gmail.com>
2025-09-21 22:25:18 +0530

    Profile image added

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
           <img
-            src="./public/profile.jpg" 
+            src="/profile.jpg" 
            className="w-45 h-45 mx-auto rounded-full mb-6 border-4 border-blue-500 shadow-lg object-cover"
           />
           <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">
